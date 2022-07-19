502779247b5df55b312a01bbcdb40cf528ca5bc2
MichalMalinowski29 <109347329+MichalMalinowski29@users.noreply.github.com>
2022-07-19 11:37:45 +0200

    Fixed exercise #1

--- zad1/ConsoleApp2/Program.cs
+++ zad1/ConsoleApp2/Program.cs
@@ -1,33 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace ConsoleApp2
 {
 
-    public class Zadania
+    public class DistanceCalculator
+    {
+        public void ConvertToKilometers()
         {
-            public void zad1()
-            {
-                Console.WriteLine("Podaj odległośc w kilometrach: ");
-                string km = Console.ReadLine();
-                Console.WriteLine("Odległość w metrach wynosi: ");
-                int zmienna = Int32.Parse(km) * 1000;
-                Console.WriteLine(zmienna);
-                File.WriteAllText("plik.txt", "Odległość w metrach wynosi: " + zmienna.ToString());
-            }
+            Console.WriteLine("Podaj odległośc w kilometrach: ");
+            string km = Console.ReadLine();
+            Console.WriteLine("Odległość w metrach wynosi: ");
+            Console.WriteLine(Int32.Parse(km) * 1000);
+            File.WriteAllText("plik.txt", "Odległość w metrach wynosi: " + (Int32.Parse(km) * 1000).ToString());
         }
-
-
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Zadania zadanie1 = new Zadania();
-            zadanie1.zad1();
+            DistanceCalculator example = new DistanceCalculator();
+            example.ConvertToKilometers();
         }
     }
 }
